d1799b3a1c6b16ba2a72859f6da90ac7a8952fa7
baronnycastro <baronnycastro@gmail.com>
2021-07-27 13:11:41 -0300

    Mudando de post para get

--- src/script.js
+++ src/script.js
@@ -5,36 +5,36 @@ function encaminhar() {
     const numero = document.getElementById("numero").value
     const telefone = document.getElementById("telefone").value
 
-    if(nome != "" && cpf != "" && logradouro != "" && numero != "" && telefone != ""){
-    window.location.href = "../resposta.html";
+    if (nome != "" && cpf != "" && logradouro != "" && numero != "" && telefone != "") {
+        window.location.href = "../resposta.html";
     }
     else {
         alert("Preencha os campos obrigatórios!")
     }
 }
- 
 
-function buscaCep(){
+
+function buscaCep() {
     const cep = document.getElementById("cep");
     const url = `http://viacep.com.br/ws/${cep.value}/json/`;
-    
+
     const logradouro = document.getElementById("logradouro");
     const bairro = document.getElementById("bairro");
     const localidade = document.getElementById("localidade");
     const uf = document.getElementById("uf");
 
-   
-    fetch(url, {method: 'GET'})
-    .then(
-        response => {
-            response.json()
-                .then(dado => {
-                    logradouro.value = dado.logradouro;
-                    bairro.value = dado.bairro;
-                    localidade.value = dado.localidade;
-                    uf.value = dado.uf;
-                })
-        })
+
+    fetch(url, { method: 'GET' })
+        .then(
+            response => {
+                response.json()
+                    .then(dado => {
+                        logradouro.value = dado.logradouro;
+                        bairro.value = dado.bairro;
+                        localidade.value = dado.localidade;
+                        uf.value = dado.uf;
+                    })
+            })
         .catch(erro => {
             logradouro.value = "";
             bairro.value = "";
