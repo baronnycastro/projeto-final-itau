edd5476603c2986fb956129e160fd91fa4a7d514
baronnycastro <baronnycastro@gmail.com>
2021-07-27 08:03:30 -0300

    arquivos projeto

---
+++ src/script.js
@@ -0,0 +1,25 @@
+function buscaCep(){
+    const cep = document.getElementById("cep");
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    
+    const logradouro = document.getElementById("logradouro");
+    const bairro = document.getElementById("bairro");
+    const localidade = document.getElementById("localidade");
+    const uf = document.getElementById("uf");
+
+    fetch(url, {method: 'GET'})
+    .then(
+        response => {
+            response.json()
+                .then(dado => {
+                    logradouro.value = dado.logradouro;
+                    bairro.value = dado.bairro;
+                    localidade.value = dado.localidade;
+                    uf.value = dado.uf;
+                })
+        })
+        .catch(erro => {
+            
+            alert("CEP não localizado")
+        })
+}
