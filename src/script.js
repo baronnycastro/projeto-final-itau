bf69736d4dedfb04aeb918b8b737fa05b7c7648e
baronnycastro <baronnycastro@gmail.com>
2021-07-27 11:34:08 -0300

    alterações no script e no index.html

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ function encaminhar() {
     const telefone = document.getElementById("telefone").value
 
     if(nome != "" && cpf != "" && logradouro != "" && numero != "" && telefone != ""){
-    window.location.href = "resposta.html";
+    window.location.href = "./resposta.html";
     }
     else {
         alert("Preencha os campos obrigatórios!")
@@ -23,6 +23,7 @@ function buscaCep(){
     const localidade = document.getElementById("localidade");
     const uf = document.getElementById("uf");
 
+   
     fetch(url, {method: 'GET'})
     .then(
         response => {
